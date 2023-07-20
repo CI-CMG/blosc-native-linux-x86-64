1c8101dbb8208bf611827248dbbe3ef113fe9385
Chris Slater <6004777+electricsam@users.noreply.github.com>
2023-07-19 19:39:37 -0600

    copy linux library

--- build.sh
+++ build.sh
@@ -11,6 +11,5 @@ cmake ..
 cmake --build . --config Release
 cd ../..
 mkdir -p src/main/resources/linux-x86-64
-ls c-blosc/build/blosc
-cp c-blosc/build/blosc/libblosc.dylib src/main/resources/linux-x86-64
+cp c-blosc/build/blosc/libblosc.so src/main/resources/linux-x86-64
 rm -rf c-blosc
